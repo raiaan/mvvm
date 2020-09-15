7500a0498fe49a12fbe872261c49d45d0053de7e
raiaan  mustfa <raiaanm@gmail.com>
2020-09-15 03:23:16 +0200

    MVVM design pattren

---
+++ app/src/main/java/com/example/mvvmtraining/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mvvmtraining;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.mvvmtraining.databinding.ActivityMainBinding;
+import com.example.mvvmtraining.viewmodal.LoginViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
+    }
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
